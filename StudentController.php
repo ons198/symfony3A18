3714955996bbce8551812717ec2effc097969182
ons198 <92670143+ons198@users.noreply.github.com>
2023-02-05 23:16:51 +0100

    Add files via upload

---
+++ StudentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    #[Route('/student', name: 'app_student')]
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+
+
+    #[Route('/welcome', name:'app_index')]
+    public function index2() : Response
+    {
+        return new Response("Bonjour mes etudiants");
+    }
+
+}
